bc2fca8c3ef51c4fa7d92ce40efbfe41418dc3b1
luispiedrahita258 <lepiedrahita5@misena.edu.co>
2021-09-30 14:33:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
-/*
- *  * File: 4-puts.c
- *   * Auth: Brennan D Baraban <375@holbertonschool.com>
- *    */
-
 #include <stdio.h>
-
 /**
- *  * main - Prints "Programming is like building a multilingual
- *   *                puzzle, followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
 }
