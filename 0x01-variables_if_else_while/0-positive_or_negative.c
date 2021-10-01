ce060b4604adc9b435efb9b3b08e21a237d06275
luispiedrahita258 <lepiedrahita5@misena.edu.co>
2021-10-01 09:42:09 -0700

    changed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include <time.h>
+
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 /**
- *  * main - main block
- *   * Description: Get a random number and print the number
- *    * and if it is positive, negative, or zero
- *     * Return: 0
- *     **/
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+ **/
 int main(void)
 {
-		int n;
+  int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 
-					if (n > 0)
-								printf("%i is positive\n", n);
-						else if (n < 0)
-									printf("%i is negative\n", n);
-							else
-										printf("%i is zero\n", n);
+  if (n > 0)
+    printf("%d is positive\n", n);
+  else if (n < 0)
+    printf("%d is negative\n", n);
+  else
+    printf("%d is zero\n", n);
 
-								return (0);
+  return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
+
 /**
- *  * main - main block
- *   * Description: Use `putchar` function to print the alphabet in lowercase.
- *    * Return: 0
- *     **/
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ **/
 int main(void)
 {
-		char c = 'a';
+  char letter;
 
-			while (c <= 'z')
-					{
-								putchar(c);
-										c++;
-											}
-				putchar('\n');
+  for (letter = 'a'; letter <= 'z'; letter++)
+    putchar(letter);
 
-					return (0);
+  putchar('\n');
+
+  return (0);
 }
