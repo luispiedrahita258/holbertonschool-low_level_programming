d9313fa662cb5a2183e42aae3da8b54d4cba5a11
luispiedrahita258 <67211947+luispiedrahita258@users.noreply.github.com>
2021-10-06 09:10:31 -0500

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,12 +5,12 @@
  * @c: The character to be checked.
  *
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
- */
+ **/
 int _isalpha(int c)
 {
-  if ((c >= 'a' && c <= 'z') ||
-      (c >= 'A' && c <= 'Z'))
-    return (1);
-  else
-    return (0);
+if ((c >= 'a' && c <= 'z') ||
+(c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
