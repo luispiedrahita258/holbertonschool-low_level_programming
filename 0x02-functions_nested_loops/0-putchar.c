6f4614e94688722e65860c428aaa2eb82c3aaef4
luispiedrahita258 <67211947+luispiedrahita258@users.noreply.github.com>
2021-10-06 09:01:45 -0500

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+ /** main - Print "_pucthar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ **/
 
 int main(void)
 {
