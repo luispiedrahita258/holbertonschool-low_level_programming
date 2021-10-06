7f8ebf5427684574abf6647a16bf2951100633d9
luispiedrahita258 <67211947+luispiedrahita258@users.noreply.github.com>
2021-10-06 09:12:52 -0500

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,28 +7,22 @@
  * Return: 1 if the number is greater than zero,
  *         0 if the number is zero,
  *         -1 if the number is less than zero.
- */
+ **/
 int print_sign(int n)
 {
-  if (n > 0)
+if (n > 0)
     {
-      _putchar('+');
-      return (1);
-    }
-  else if (n == 0)
-    {
-      _putchar('0');
-      return (0);
-    }
-  else
-    {
-      _putchar('-');
-      return (-1);
-    }
+_putchar('+');
+return (1);
 }
-2021 GitHub, Inc.
-  Terms
-  Privacy
-  Security
-  Status
-  D
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
