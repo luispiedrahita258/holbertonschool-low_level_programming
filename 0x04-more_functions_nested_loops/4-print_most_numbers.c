424d41d65def578aa0ecc36037c84d5c99e6013e
luispiedrahita258 <lepiedrahita5@misena.edu.co>
2021-10-08 19:17:40 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,19 +9,19 @@
  */
 void print_most_numbers(void)
 {
-  int x = 0;
+int x = 0;
 
-  for (; x <= 9; x++)
-    {
-      if (x == 2 || x == 4)
-	{
-	  continue;
-	}
-      else
-	{
-	  _putchar(x + '0');
-	}
-    }
+for (; x <= 9; x++)
+{
+if (x == 2 || x == 4)
+{
+continue;
+}
+else
+{
+_putchar(x + '0');
+}
+}
 
-  _putchar('\n');
+_putchar('\n');
 }
