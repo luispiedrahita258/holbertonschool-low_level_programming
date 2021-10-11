c356eebeabe2eb5296f7750aacf590a7221185ed
luispiedrahita258 <lepiedrahita5@misena.edu.co>
2021-10-11 12:20:29 -0500

    changed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,16 @@
  */
 
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-int index = 0;
+int i;
 
-while (src[index])
+for (i = 0; src[i] != '\0'; i++)
 {
-dest[index] = src[index];
-index++;
+dest[i] = src[i];
 }
+
+dest[i++] = '\0';
 
 return (dest);
 }
