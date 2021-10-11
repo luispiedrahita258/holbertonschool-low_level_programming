49c67fceb3d777d74176763c85ce286a987edf65
luispiedrahita258 <lepiedrahita5@misena.edu.co>
2021-10-11 12:42:47 -0500

    changed

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,45 +10,34 @@
  **/
 int main(void)
 {
-char password[84];
-int index = 0, sum = 0, diff_half1, diff_half2;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-srand(time(0));
-
-while (sum < 2772)
-{
-password[index] = 33 + rand() % 94;
-sum += password[index++];
-}
-
-password[index] = '\0';
-
-if (sum != 2772)
-{
-diff_half1 = (sum - 2772) / 2;
-diff_half2 = (sum - 2772) / 2;
-if ((sum - 2772) % 2 != 0)
-diff_half1++;
-
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half1))
-{
-password[index] -= diff_half1;
-break;
-}
-}
-for (index = 0; password[index]; index++)
-{
-if (password[index] >= (33 + diff_half2))
-{
-password[index] -= diff_half2;
-break;
-}
-}
-}
-
-printf("%s", password);
-
-return (0);
+	srand(time(NULL));
+	while (s != 2772)
+	{
+		i = k = s = 0;
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
+	}
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
 }
