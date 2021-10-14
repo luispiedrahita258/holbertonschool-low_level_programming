52445fe42e5835f973c3863adf45388502d9a7f2
luispiedrahita258 <67211947+luispiedrahita258@users.noreply.github.com>
2021-10-13 20:16:48 -0500

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ **/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int add = 0, len1, len2, i, j;
+
+for (len1 = 0; n1[len1]; len1++)
+;
+for (len2 = 0; n2[len2]; len2++)
+;
+if (len1 > size_r || len2 > size_r)
+return (0);
+len1--;
+len2--;
+size_r--;
+for (i = 0; i < size_r; i++, len1--, len2--)
+{
+if (len1 >= 0)
+add += n1[len1] - '0';
+if (len2 >= 0)
+add += n2[len2] - '0';
+if (len1 < 0 && len2 < 0 && add == 0)
+break;
+r[i] = add % 10 + '0';
+add /= 10;
+}
+r[i] = '\0';
+if (len1 >= 0 || len2 >= 0 || add)
+return (0);
+for (i--, j = 0; i > j; i--, j++)
+{
+add = r[i];
+r[i] = r[j];
+r[j] = add;
+}
+return (r);
+}
+
